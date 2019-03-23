c6aff8812a9b72d49b91063425ad84dc44d16bf5
Grant Payne <47309289+grantpayne@users.noreply.github.com>
2019-03-23 14:39:18 -0400

    initial commit

---
+++ CoreIntroduction/CoreIntroduction/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CoreIntroduction.Models;
+
+namespace CoreIntroduction.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //replace with your codez
+            throw new NotImplementedException();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            //replace with your codez
+            throw new NotImplementedException();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            //replace with your codez
+            throw new NotImplementedException();
+        }
+
+        public IActionResult Privacy()
+        {
+            //replace with your codez
+            throw new NotImplementedException();
+        }
+
+        //Don't worry about this:
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
